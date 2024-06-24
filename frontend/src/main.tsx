c2823c38d9e3602c6ac1f082473c9df600488c99
mk-github-1 <32920703+mk-github-1@users.noreply.github.com>
2024-06-24 12:46:55 +0900

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './assets/index.css'
 
@@ -10,6 +11,8 @@ import './assets/index.css'
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
